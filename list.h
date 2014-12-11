6df0361380387f86b5e9ab169fda8daf108b81c7
brijeshmeshiya <meshiya_brijesh@yahoo.com>
2014-12-11 10:45:47 +0530

    Added function find_tail

--- list.h
+++ list.h
@@ -37,3 +37,26 @@ t->next=*h;
 }
 return ;
 }
+
+/* This Function finds tail in link list
+   Argument 1) : h = (head of linked list)
+   	    2) : t = (tail of linked list)
+*/
+void find_tail(struct node *h,struct node **t)
+{
+if(h==NULL)
+{
+cout<<"Can't Find Tail Since Link list is empty"<<endl;
+*t=NULL;
+}
+else
+{
+while(h->next!=NULL)
+{
+h=h->next;
+}
+*t=h;
+}
+return ;
+}
+
