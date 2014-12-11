e3e144d965ec5003adb9ad83a1ff7adb96c65448
brijeshmeshiya <meshiya_brijesh@yahoo.com>
2014-12-11 06:23:50 +0530

    Added function remove

--- list.h
+++ list.h
@@ -82,4 +82,42 @@ tail->next=t;
 return;
 }
 
+/* This functions delete node from linked list
+   Argument : 1) h= (head of link list) - May Modifies head Pointer
+              2) d= (data to delete - some integer value)
+*/
+void remove(struct node **h,int d)
+{
+if(*h==NULL)
+{
+cout<<"Can't Delete Node From Empty Link List"<<endl;
+}
+else if((*h)->data==d)
+{
+struct node *t = *h;
+cout<<"Node Is deleted with data : "<<d<<" And Head Pointer is changed"<<endl;
+*h = (*h)->next;
+free(t);
+}
+else
+{
+struct node *t= *h;
+while(t->next!=NULL && t->next->data!=d)
+{
+t=t->next;
+}
+if(t->next!=NULL && t->next->data==d)
+{
+cout<<"Node with data : "<<d<<" Is Deleted"<<endl;
+struct node *temp=t->next;
+t->next=temp->next;
+free(temp);
+}
+else
+{
+cout<<"No Node Found With Data : "<<d<<endl;
+}
+return ;
+}
+}
 
